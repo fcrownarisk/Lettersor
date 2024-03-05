3c29456183b3e84116f50bdce06f984e1cd6da03
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-03-04 19:25:42 -0800

    Add files via upload

---
+++ .idea/fenymann diagram/I4.ts
@@ -0,0 +1,9 @@
+import '../equation/momentum'
+import '../string/Momentum'
+import '../fenymann diagram/fenymann diagram'
+import '../scatter.css'
+void function momentum(){
+    const I4=
+    'p1'+'p2'=='p3'+'p4'
+    return 'scatter.css'
+}
