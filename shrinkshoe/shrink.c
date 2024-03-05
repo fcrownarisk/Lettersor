ec431f9a1b13d5dd5d59a1129f0dcc9ee3822206
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-03-05 00:38:36 -0800

    Add files via upload

---
+++ shrinkshoe/shrink.c
@@ -0,0 +1,29 @@
+class123{
+int i,j,k
+}
+class456{
+int l,m,n
+}
+class789{
+int o,p,q
+}
+class1{class2{class3}}
+class4{class5{class6}}
+class7{class8{class9}}
+type void interface(){
+   [i][j][k]
+   [l][m][n]
+   [o][p][q]
+}:{
+  [class1][class2][class3]
+  [class4][class5][class6]
+  [class7][class8][class9]
+}
+type void namespace(){
+  [a][b][c][d]
+  [e][f][g][h]
+}
+type void zzzz(){
+    zzzz = z + z^2 / z^(-1)+z^(-2)
+}
+console.log(interface,namespace,zzzz);
