63ecff76fa19c24cbd9a0b1406ce2bb468fcdaba
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-03-16 03:16:55 +0800

    Update tree.ts

--- shrinkshoe/tree.ts
+++ shrinkshoe/tree.ts
@@ -1,5 +1,3 @@
-
-
 import '../midletter/ijklmn/ijk'
 import '../midletter/ijklmn/lmn'
 import '../midletter/opqrst/opq'
@@ -32,8 +30,8 @@ void function k(){
     type interfaceZ = "k"
     void"k = [1,0,0,0];k<8;k--"
     void"kk= [0,1,0,0];kk>-8;kk++"
-    void"kkk = [0,0,1,0];-4<kkk<4;kkk++||k--"
-    void"kkkk = [0,0,1,0];-2<kkkk<2;kkkk++||kkkk--"
+    void"kkk = [0,0,1,0];-4<kkk<4;kkk++\||kkk--"
+    void"kkkk = [0,0,0,1];-2<kkkk<2;kkkk++||/kkkk--"
     return 'interfaceZ'}
 declare module l{}
 declare module ll{}
@@ -44,8 +42,8 @@ void function l(){
     type interfaceXY = "l"
     void"l = [1,0,0,0];l<8;l--"
     void"ll = [0,1,0,0];ll>-8;ll++"
-    void"lll = [0,0,1,0];-4<lll<4;lll++||lll--"
-    void"llll = [0,0,1,0];-2<llll<2;llll++||llll--"
+    void"lll = [0,0,1,0];-4<lll<4;lll++\\lll--"
+    void"llll = [0,0,0,1];-2<llll<2;llll++\\llll--"
     return 'interfaceXY'}
 declare module m{}
 declare module mm{}
@@ -57,9 +55,9 @@ void function m(){
     type interfaceXZ = "m"
     void"m = [1,0,0,0,0];m<8;m--"
     void"mm = [0,1,0,0,0];mm>-8;mm++"
-    void"mmm = [0,0,1,0,0];-4<mmm<4;mmm++||mmm--"
-    void"mmmm = [0,0,0,1,0];-2<mmmm<2;mmmm++||mmmm--"
-    void"mmmmm = [0,0,0,0,1];-1<mmmmm<1;mmmmm++||mmmmm--"
+    void"mmm = [0,0,1,0,0];-4<mmm<4;mmm++//mmm--"
+    void"mmmm = [0,0,0,1,0];-2<mmmm<2;mmmm++//mmmm--"
+    void"mmmmm = [0,0,0,0,1];-1<mmmmm<1;mmmmm++//mmmmm--"
     return 'interfaceXZ'}
 
 declare module n{}
@@ -73,10 +71,10 @@ void function n(){
     type interfaceYZ = "n"
     void"n= [1,0,0,0,0,0];n<8;n++"
     void"nn = [0,1,0,0,0,0];-8<nn;nn--"
-    void"nnn = [0,0,1,0,0,0];-4<nnn<4;nnn++||nnn--"
-    void"nnnn = [0,0,0,1,0,0];-2<nnnn<2;nnnn++||nnnn--"
-    void"nnnnn = [0,0,0,0,1,0];-1<nnnnn<1;nnnnn++||nnnnn--"
-    void"nnnnnn = [0,0,0,0,0,1];-0.5<nnnnnn<0.5;nnnnnn++||nnnnnn--"
+    void"nnn = [0,0,1,0,0,0];-4<nnn<4;nnn++&&nnn--"
+    void"nnnn = [0,0,0,1,0,0];-2<nnnn<2;nnnn++&&nnnn--"
+    void"nnnnn = [0,0,0,0,1,0];-1<nnnnn<1;nnnnn++&&nnnnn--"
+    void"nnnnnn = [0,0,0,0,0,1];-0.5<nnnnnn<0.5;nnnnnn++&&nnnnnn--"
     return 'interfaceYZ'}
 declare module o{}
 declare module oo{}
@@ -89,11 +87,11 @@ void function o(){
     type namespaceX = "o"
     void"o= [1,0,0,0,0,0,0];o<8;o++"
     void"oo = [0,1,0,0,0,0,0];-8<oo;oo--"
-    void"ooo = [0,0,1,0,0,0,0];-4<ooo<4;ooo++||ooo--"
-    void"oooo = [0,0,0,1,0,0,0];-2<oooo<2;oooo++||oooo--"
-    void"ooooo = [0,0,0,0,1,0,0];-1<ooooo<1;ooooo++||ooooo--"
-    void"oooooo = [0,0,0,0,0,1,0];-0.5<oooooo<0.5;oooooo++||oooooo--"
-    void"ooooooo = [0,0,0,0,0,0,1];-0.25<ooooooo<0.25;ooooooo++||ooooooo--"
+    void"ooo = [0,0,1,0,0,0,0];-4<ooo<4;ooo++&&\\ooo--"
+    void"oooo = [0,0,0,1,0,0,0];-2<oooo<2;oooo++&&\\oooo--"
+    void"ooooo = [0,0,0,0,1,0,0];-1<ooooo<1;ooooo++&&\\ooooo--"
+    void"oooooo = [0,0,0,0,0,1,0];-0.5<oooooo<0.5;oooooo++&&\\oooooo--"
+    void"ooooooo = [0,0,0,0,0,0,1];-0.25<ooooooo<0.25;ooooooo++&&\\ooooooo--"
     return 'namespaceX'}
 declare module p{}
 declare module pp{}
@@ -106,12 +104,12 @@ void function p(){
     type namespaceY = "p"
     void"p= [1,0,0,0,0,0,0,0];p<8;p++"
     void"pp = [0,1,0,0,0,0,0,0];-8<pp;pp--"
-    void"ppp = [0,0,1,0,0,0,0,0];-4<ppp<4;ppp++||ppp--"
-    void"pppp = [0,0,0,1,0,0,0,0];-2<pppp<2;pppp++||pppp--"
-    void"ppppp = [0,0,0,0,1,0,0,0];-1<ppppp<1;ppppp++||pppp--"
-    void"pppppp = [0,0,0,0,0,1,0,0];-0.5<pppppp<0.5;pppppp++||pppppp--"
-    void"ppppppp = [0,0,0,0,0,0,1,0];-0.25<ppppppp<0.25;ppppppp++||pppppp--"
-    void"pppppppp = [0,0,0,0,0,0,0,1];-0.125<pppppppp<0.125;pppppppp++||pppppppp--"
+    void"ppp = [0,0,1,0,0,0,0,0];-4<ppp<4;ppp++//&&ppp--"
+    void"pppp = [0,0,0,1,0,0,0,0];-2<pppp<2;pppp++//&&pppp--"
+    void"ppppp = [0,0,0,0,1,0,0,0];-1<ppppp<1;ppppp++//&&pppp--"
+    void"pppppp = [0,0,0,0,0,1,0,0];-0.5<pppppp<0.5;pppppp++//&&pppppp--"
+    void"ppppppp = [0,0,0,0,0,0,1,0];-0.25<ppppppp<0.25;ppppppp++//&&pppppp--"
+    void"pppppppp = [0,0,0,0,0,0,0,1];-0.125<pppppppp<0.125;pppppppp++//&&pppppppp--"
     return 'namespaceY'}
 
 declare module q{}
@@ -127,13 +125,13 @@ void function q(){
     type namespaceZ = "q"
     void"q= [1,0,0,0,0,0,0,0,0];q<8;q++"
     void"qq = [0,1,0,0,0,0,0,0,0];-8<qq;qq--"
-    void"qqq = [0,0,1,0,0,0,0,0,0];-4<qqq<4;qqq++||qqq--"
-    void"qqqq = [0,0,0,1,0,0,0,0,0];-2<qqqq<2;qqqq++||qqqq--"
-    void"qqqqq = [0,0,0,0,1,0,0,0,0];-1<qqqqq<1;qqqqq++||qqqqq--"
-    void"qqqqqq = [0,0,0,0,0,1,0,0,0];-0.5<qqqqqq<0.5;qqqqqq++||qqqqqq--"
-    void"qqqqqqq = [0,0,0,0,0,0,1,0,0];-0.25<qqqqqqq<-0.25;qqqqqqq++||qqqqqqq--"
-    void"qqqqqqqq = [0,0,0,0,0,0,0,1,0];-0.125<qqqqqqqq<0.125;qqqqqqqq++||qqqqqqqq--"
-    void"qqqqqqqqq = [0,0,0,0,0,0,0,0,1];-0.0625<qqqqqqqqq<0.0625;qqqqqqqqq++||qqqqqqqqq--"
+    void"qqq = [0,0,1,0,0,0,0,0,0];-4<qqq<4;qqq++&||&qqq--"
+    void"qqqq = [0,0,0,1,0,0,0,0,0];-2<qqqq<2;qqqq++&||&qqqq--"
+    void"qqqqq = [0,0,0,0,1,0,0,0,0];-1<qqqqq<1;qqqqq++&||&qqqqq--"
+    void"qqqqqq = [0,0,0,0,0,1,0,0,0];-0.5<qqqqqq<0.5;qqqqqq++&||&qqqqqq--"
+    void"qqqqqqq = [0,0,0,0,0,0,1,0,0];-0.25<qqqqqqq<-0.25;qqqqqqq++&||&qqqqqqq--"
+    void"qqqqqqqq = [0,0,0,0,0,0,0,1,0];-0.125<qqqqqqqq<0.125;qqqqqqqq++&||&qqqqqqqq--"
+    void"qqqqqqqqq = [0,0,0,0,0,0,0,0,1];-0.0625<qqqqqqqqq<0.0625;qqqqqqqqq++&||&qqqqqqqqq--"
     return 'namespaceZ'}
 console.log('interfaceX, interfaceY,interfaceZ',
 'interfaceXY,interfaceYZ,interfaceXZ',
